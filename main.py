c03383a415672aba64f7ac06ef3084484bb89a4e
qiray <zotovyaa@mail.ru>
2020-12-03 16:49:58 +0300

    Added parse_files into single text file

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 #!/bin/python3
 
-import network
-import zipfile
-import shutil
 import datetime
+import network
+import os
+import shutil
+import zipfile
+
+OUTFILE = "data.txt"
 
 # TODOlist:
 # TODO:
-# parse downloaded news into text
 # use MarkovTextGenerator to parse text to database
 # generate great news
 # post it every hour
 # clear old data and repeat on a new day
+
+def parse_files(dirname):
+    files = os.listdir(dirname)
+    outfile = open(OUTFILE, 'w') 
+    for filename in files:
+        with open(dirname + "/" + filename) as f:
+            next(f)
+            for line in f:
+                data = line.split("\t")
+                value = data[1].replace("&quot;", "\"")
+                outfile.write(value + "\n")
+    outfile.close()
 
 def main():
     dirname = "rawdata"
@@ -21,6 +35,7 @@ def main():
     network.download_file("http://mediametrics.ru/data/archive/day/ru-{}.zip".format(yesterday), filename)
     with zipfile.ZipFile(filename, 'r') as zip_ref:
         zip_ref.extractall(dirname)
+    parse_files(dirname + "/day")
 
 if __name__ == '__main__':
     main()
